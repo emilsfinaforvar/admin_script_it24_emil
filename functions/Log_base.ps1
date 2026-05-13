3365db0a1b6884aa527e7e81f3d19e93e4350b3d
DESKTOP-12CJN77\Emil <66754@gymnasium.ax>
2026-05-13 14:02:42 +0300

    Shows cpu stats for computer

--- functions/Log_base.ps1
+++ functions/Log_base.ps1
@@ -6,6 +6,7 @@ $Computername = $env:Computername
 Write-Output "The Computer is named $Computername"
 $Systeminfo = Get-Ciminstance Win32_Operatingsystem|
 Select-Object Caption,OSArchitecture,Version
-New-Item -Path . -Name "logfile.log" -ItemType "File" -Value "Loginfo from commands"
+Get-Process | Measure-Object -Average -Sum  -Maximum CPU
+New-Item -Path "..\logs\" -Name "logfile.log" -ItemType "File" -Value "Loginfo from commands"
 $Log = "$env:Computername - $Time - $Username - $Systeminfo"
 $Log | Add-Content "..\logs\logfile.log"
